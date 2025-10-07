4c82decffc230e1295a22f8cc61664423c844bf2
Yuri Abele <Yuri.Abele@exxeta.com>
2025-10-07 18:13:13 +0200

    Added HouseholdChemicals to cart

--- classes/products/__init__.py
+++ classes/products/__init__.py
@@ -1,2 +1,3 @@
 from .clothing import Clothing
 from .electronics import Electronics
+from .household_chemicals import HouseholdChemicals
--- shop.py
+++ shop.py
@@ -1,12 +1,11 @@
-from classes.products import Clothing
-from classes.products import Electronics
+from classes.products import Clothing, Electronics, HouseholdChemicals
 from classes.shoping_cart import ShoppingCart
-from classes.users import Admin
-from classes.users import Customer
+from classes.users import Admin, Customer
 
 # Создаем продукты
 laptop = Electronics(name="Ноутбук", price=120000, brand="Dell", warranty_period=2)
 tshirt = Clothing(name="Футболка", price=200, size="M", material="Хлопок")
+fairy = HouseholdChemicals(name="Fairy", price=150)
 
 # Создаем пользователей
 customer = Customer(username="Mikhail", email="python@derkunov.ru", address="033 Russ Bur")
@@ -16,6 +15,7 @@ admin = Admin(username="root", email="root@derkunov.ru", admin_level=5)
 cart = ShoppingCart()
 cart.add_item(laptop, 1)
 cart.add_item(tshirt, 3)
+cart.add_item(fairy, 2)
 
 # Выводим детали корзины
 print(cart.get_details())
